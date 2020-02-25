3189c90401233746ef81389b3516a33099eab8d9
npetrov937 <60854053+npetrov937@users.noreply.github.com>
2020-02-25 17:35:12 +0000

    added input files and updates to gui

--- GUI only.py
+++ GUI only.py
@@ -124,15 +124,18 @@ alpha_threshold_tk.set("0.05")
 set_variables_default()
 
 input_filename_Frame = tk.LabelFrame(master, text="Choose input file", padx=5, pady=5, borderwidth=3)
-input_filename_Frame.grid(row=1, column=0, columnspan=50)
+input_filename_Frame.grid(row=1, column=0, columnspan=50, sticky="W", pady=15, padx=15)
 
 input_type_Frame = tk.LabelFrame(master, text="Select input type:", padx=5, pady=5, borderwidth=3)
-input_type_Frame.grid(row=2,column=0,rowspan=6, sticky='W')
+input_type_Frame.grid(row=2,column=0, rowspan=45, sticky="W", padx=15)
 
 raw_test_Frame = tk.LabelFrame(master, text="Statistical test", padx=5, pady=5, borderwidth=3)
-raw_test_Frame.grid(row=2, column=1, rowspan=1, stick='W')
+raw_test_Frame.grid(row=2, column=1, sticky="W")
 
-raw_corr_Frame = tk.Frame(master)
+raw_corr_Frame = tk.LabelFrame(master, text="Correlation type", padx=5, pady=5, borderwidth=3)
+raw_corr_Frame.grid(row=3, column=1, sticky="W")
+
+
 raw_mr_Frame = tk.Frame(master)
 raw_indttest_Frame = tk.Frame(master)
 raw_pairttest_Frame = tk.Frame(master)
@@ -141,16 +144,20 @@ summ_corr_varNames_Frame = tk.Frame(master)
 
 summ_indttest_cols_Frame = tk.Frame(master)
 
-spss_test_tk_Frame = tk.Frame(master)
+spss_test_Frame = tk.LabelFrame(master, text="Statistical test", padx=5, pady=5, borderwidth=3)
+spss_test_Frame.grid(row=8, column=1, sticky="W")
 spss_indttest_Frame = tk.Frame(master)
 spss_pairttest_Frame = tk.Frame(master)
 
-effect_size_Frame = tk.Frame(master)
-correction_type_Frame = tk.Frame(master)
+effect_size_Frame = tk.LabelFrame(master, text="Effect size", padx=5, pady=5, borderwidth=3)
+effect_size_Frame.grid(row=4, column=1, sticky="W")
+correction_type_Frame = tk.LabelFrame(master, text="Correction type", padx=5, pady=5, borderwidth=3)
+correction_type_Frame.grid(row=5, column=1, sticky="W")
 
-raw_ttest_output_descriptives_Frame = tk.Frame(master)
-non_numeric_input_raise_errors_Frame = tk.Frame(master)
-pvalues_type_Frame = tk.Frame(master)
+raw_ttest_output_descriptives_Frame = tk.LabelFrame(master, text="Output descriptive statistics?", padx=5, pady=5, borderwidth=3)
+raw_ttest_output_descriptives_Frame.grid(row=6, column=1, sticky="W")
+non_numeric_input_raise_errors_Frame = tk.LabelFrame(master, text="How to handle non-numeric cases?", padx=5, pady=5, borderwidth=3)
+non_numeric_input_raise_errors_Frame.grid(row=7, column=1, sticky="W")
 
 #-----------------2.2. Layout control
 def remove_frames(frames_list):
@@ -191,9 +198,9 @@ def select_file():
 	elif filename != "":
 		messagebox.showerror("Error!", "Please select a valid file - XLSX only.")
 
-tk.Button(input_filename_Frame, text="Select input file (xlsx only)", command=select_file).grid(row=0, column=0, sticky='W', padx=(0, 10))
+tk.Button(input_filename_Frame, text="Select input file (xlsx only)", command=select_file).grid(row=0, column=0, sticky="W", padx=(0, 10))
 filename_label = tk.Label(input_filename_Frame, text="No file selected.")
-filename_label.grid(row=0, column=1, columnspan=1, sticky='W')
+filename_label.grid(row=0, column=1, columnspan=1, sticky="W")
 
 #Input Type
 input_type_Dict = {"Raw data" : "raw", "Correlations from\nsummary statistics" : "summ_corr", 
@@ -201,14 +208,67 @@ input_type_Dict = {"Raw data" : "raw", "Correlations from\nsummary statistics" :
 					"P values" : "pvalues"}
 i_input = 0 #used for rows in the grid
 for (text, value) in input_type_Dict.items(): 
-		tk.Radiobutton(input_type_Frame, text=text, variable=input_type_tk, value=value, command=input_type_frames_layout).grid(row=i_input,sticky='W')
+		tk.Radiobutton(input_type_Frame, text=text, variable=input_type_tk, value=value, command=input_type_frames_layout).grid(row=i_input,sticky="W")
 		i_input += 1
 
 #Raw data // test
+raw_corr_options = ("Select a correlation...", "Pearson's r", "Spearman's rho", "Kendall's tau")
+raw_corr_drop = ttk.Combobox(raw_corr_Frame, values=raw_corr_options, state="readonly", width=30, textvariable=raw_corr_type_tk)
+raw_corr_drop.current(0)
+raw_corr_drop.grid(row=0, column=0)
+
+#Raw data // test // Correlations
 raw_test_options = ("Select a test...", "Correlations", "(Multiple) Regression - Standard", "Independent Samples t-test", "Paired samples t-test")
 raw_test_drop = ttk.Combobox(raw_test_Frame, values=raw_test_options, state="readonly", width=30, textvariable=raw_test_tk)
 raw_test_drop.current(0)
 raw_test_drop.grid(row=0, column=0)
+
+#Raw data // test // Multiple Regression
+
+#Raw data // test // Independent samples t-test
+
+#Raw data // test // Paired samples t-test
+
+#Summary statistics - Correlations
+
+#Summary statistics - Independent samples t-test
+
+#SPSS // test
+spss_test_options = ("Select a test...", "Correlations", "(Multiple) Regression - Standard", "Independent Samples t-test", "Paired samples t-test")
+spss_test_drop = ttk.Combobox(spss_test_Frame, values=spss_test_options, state="readonly", width=30, textvariable=spss_test_tk)
+spss_test_drop.current(0)
+spss_test_drop.grid(row=0, column=0)
+
+#SPSS // test // Independent samples t-test
+
+#SPSS // test // Paired samples t-test
+
+#Effect size choice
+effect_size_options = ("Select effect size...", "Cohen's d", "Hedge's g", "Glass's delta", "None")
+effect_size_drop = ttk.Combobox(effect_size_Frame, values=effect_size_options, state="readonly", width=30, textvariable=effect_size_choice_tk)
+effect_size_drop.current(0)
+effect_size_drop.grid(row=0, column=0)
+
+#Correction type choice
+correction_type_options = ("Select correction...", "Bonferroni",  "Sidak", "Holm-Sidak", "Holm-Bonferroni", "Simes-Hochberg", "Hommel", 
+						"Benjamini-Hochberg", "Benjamini-Yekutieli", "Benjamini-Hochberg (2-stage) ", 
+						"Benjamini-Yekutieli (2-stage)", "None")
+correction_type_drop = ttk.Combobox(correction_type_Frame, values=correction_type_options, state="readonly", width=30, textvariable=correction_type_tk)
+correction_type_drop.current(0)
+correction_type_drop.grid(row=0, column=0)
+
+#Pvalues choice
+
+#Raise errors on non-numeric input choice
+non_numeric_errors_options = ("Select error handling...", "Raise errors", "Ignore case-wise")
+non_numeric_errors_drop = ttk.Combobox(non_numeric_input_raise_errors_Frame, values=non_numeric_errors_options, state="readonly", width=30, textvariable=non_numeric_input_raise_errors_tk)
+non_numeric_errors_drop.current(0)
+non_numeric_errors_drop.grid(row=0, column=0)
+
+#Output descriptives for Raw t-tests
+raw_ttest_output_descriptives_drop = ttk.Combobox(raw_ttest_output_descriptives_Frame, values=["No", "Yes"], state="readonly", width=30, textvariable=raw_ttest_output_descriptives_tk)
+raw_ttest_output_descriptives_drop.current(0)
+raw_ttest_output_descriptives_drop.grid(row=0, column=0)
 
 
 #-----------------2.4. On Submit functions
